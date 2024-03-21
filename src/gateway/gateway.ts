b13a7dc8f5064633a359e411646a35e078893283
ASonneP <sonnep.dev@gmail.com>
2024-03-22 05:16:01 +0700

    :sparkles: feat: Implement WebSocket event handlers for joining and leaving rooms

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,12 +1,11 @@
-import { OnModuleInit } from '@nestjs/common';
-import {
-  MessageBody,
-  SubscribeMessage,
-  WebSocketGateway,
-  WebSocketServer,
-} from '@nestjs/websockets';
+import { OnModuleInit, Logger } from '@nestjs/common';
+import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody, ConnectedSocket } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
-import { Server } from 'socket.io';
+interface User {
+  id: string;
+  username: string;
+}
 
 @WebSocketGateway({
   cors: {
@@ -17,32 +16,57 @@ export class MyGateWay implements OnModuleInit {
   @WebSocketServer()
   server: Server;
 
-  userCount = 0;
+  private logger: Logger = new Logger('MyGateway');
+  private rooms: { [roomName: string]: User[] } = {}; // Object to store users in each room
 
   onModuleInit() {
-    this.server.on('connection', (socket) => {
-      console.log(socket.id);
-      console.log('connected');
-
-      this.userCount++; // Increment user count when a new user connects
-      this.emitUserCount();
-      socket.on('disconnect', () => {
-        this.userCount--; // Decrement user count when a user disconnects
-        this.emitUserCount(); // Emit updated user count to all clients
-      });
-    });
+    this.logger.log('WebSocket gateway initialized');
   }
-  emitUserCount() {
-    this.server.emit('userCount', this.userCount); // Emit user count to all clients
+
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(@MessageBody() data: { room: string, username: string }, @ConnectedSocket() client: Socket) {
+    const { room, username } = data;
+
+    client.join(room);
+    this.logger.log(`Client ${client.id} joined room: ${room}`);
+    
+    // Add user to the room
+    if (!this.rooms[room]) {
+      this.rooms[room] = [{ id: client.id, username }];
+    } else {
+      this.rooms[room].push({ id: client.id, username });
+    }
+
+    // Emit user count and user list to all clients in the room
+    this.server.to(room).emit('userCount', this.rooms[room].length);
+    this.server.to(room).emit('userList', this.rooms[room]);
+    this.server.to(room).emit('userJoined', { id: client.id, username, room });
+  }
+
+  @SubscribeMessage('leaveRoom')
+  handleLeaveRoom(@MessageBody() room: string, @ConnectedSocket() client: Socket) {
+    client.leave(room);
+    this.logger.log(`Client ${client.id} left room: ${room}`);
+
+    // Remove user from the room
+    if (this.rooms[room]) {
+      this.rooms[room] = this.rooms[room].filter(user => user.id !== client.id);
+
+      // Emit updated user count and user list to all clients in the room
+      this.server.to(room).emit('userCount', this.rooms[room].length);
+      this.server.to(room).emit('userList', this.rooms[room]);
+    }
+    // Update room participants or notify other members as needed
   }
 
   @SubscribeMessage('newMessage')
-  onNewMessage(@MessageBody() body: any) {
-    console.log(body);
-    this.server.emit('onMessage', {
+  handleNewMessage(@MessageBody() data: { room: string; message: string }, @ConnectedSocket() client: Socket) {
+    const { room, message } = data;
+    this.logger.log(`New message in room ${room}: ${message}`);
+    this.server.to(room).emit('onMessage', {
       msg: 'New Message',
-      content: body,
-      onlineUser: this.userCount,
+      content: message,
+      senderId: client.id,
     });
   }
 }
